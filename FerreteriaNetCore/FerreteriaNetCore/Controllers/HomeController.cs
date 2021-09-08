98c2e11499dd3d5157ba176f32e3a8eb864cad14
Emmanuel Carron <diego.carron@mercadolibre.com>
2021-09-08 20:58:56 -0300

    Redireccionamiento desde controller a Password recovery view

--- FerreteriaNetCore/FerreteriaNetCore/Controllers/HomeController.cs
+++ FerreteriaNetCore/FerreteriaNetCore/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace FerreteriaNetCore.Controllers
             return View();
         }
 
+        public IActionResult PasswordRecovery()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
