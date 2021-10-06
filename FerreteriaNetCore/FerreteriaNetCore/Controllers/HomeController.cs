6d3256428a8bcc7434ee965a317868610f99b2c2
Emmanuel <emmanuel.carron@itscordoba.edu.ar>
2021-10-06 18:51:25 -0300

    Redireccionamiento diferente

--- FerreteriaNetCore/FerreteriaNetCore/Controllers/HomeController.cs
+++ FerreteriaNetCore/FerreteriaNetCore/Controllers/HomeController.cs
@@ -34,7 +34,7 @@ namespace FerreteriaNetCore.Controllers
 
         public IActionResult Search()
         {
-            return View();
+            return View("~/Views/Home/ProductSearch.cshtml");
         }
 
         public IActionResult ProductEdit()
